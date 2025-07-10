0f790994c576fc43a469b2258f621c1c0f92ff8c
AnnaVo0 <annavo45@gmail.com>
2025-07-09 23:42:25 -0400

    added start screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,30 +4,45 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [showStartScreen, setStartScreen] = useState(true);
   const [count, setCount] = useState(0)
+
+  const hideStartScreen = () => {
+    setStartScreen(false);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {showStartScreen ? (
+        <div className="start screen">
+          <h1>Street Pathfinding Visualizer</h1>
+          <h2>find your shortest path...</h2>
+          <button onClick={hideStartScreen}>start</button>
+        </div>
+      ) : (
+        <div className="main screen">
+          <div>
+            <a href="https://vite.dev" target="_blank">
+              <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+              <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+          </div>
+          <h1>Vite + React</h1>
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </div>
+      )}
     </>
   )
 }
