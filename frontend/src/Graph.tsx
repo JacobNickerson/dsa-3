d4322ce95be9f79eca6813f877aa30db07aea7e1
Jacob Nickerson <jacobnickerson817@gmail.com>
2025-08-04 00:05:00 -0400

    Fix processing order, add a broken implementation of Dijkstra's

--- frontend/src/Graph.tsx
+++ frontend/src/Graph.tsx
@@ -105,7 +105,7 @@ export class Graph {
 
         while (!q.isEmpty()) {
             const [curr,parent_node] = q.dequeue()!;
-            processing_order.push([curr,parent_node]);
+            processing_order.push([parent_node,curr]);
 
             const neighbors = this.getNeighbors(curr) ?? [];
 
@@ -160,7 +160,7 @@ export class Graph {
 
         while (!st.isEmpty()) {
             const [curr,parent_node] = st.pop()!;
-            processing_order.push([curr,parent_node]);
+            processing_order.push([parent_node,curr]);
 
             const neighbors = this.getNeighbors(curr) ?? [];
 
@@ -197,4 +197,82 @@ export class Graph {
 
         return { processing_order, path, run_time, total_weight };
     }
+
+    pathfindDijkstra(start: [number,number], end: [number,number]): PathfindResult {
+        const processing_order: Array<[FLNode,FLNode]> = [];
+        const path: Array<[FLNode,FLNode]> = [];
+
+        const visited = new Set<number>();
+        const distance_map = new Map<number,[number,FLNode?]>(); // node_id: [curr_dist,parent]
+
+        const comp = (a:[[FLNode,number],FLNode] , b:[[FLNode,number],FLNode]): number => {
+            return b[0][1] - a[0][1];
+        };
+        const pq = new PriorityQueue<[[FLNode,number],FLNode]>(comp); // goofy ahhh typing: [[node,weight],parent] (for tracking processing order)
+
+        const start_time = performance.now();
+
+        const [start_node, end_node] = this.findClosestNodes(start,end);
+
+        for (const [node_id,_] of this.nodes) {
+            distance_map.set(node_id,[Number.MAX_VALUE,undefined]);
+        } 
+
+        distance_map.set(start_node.id,[0,undefined]);
+        pq.enqueue([[start_node,0],start_node]); // start node is its own parent. sue me
+    
+        while (!pq.isEmpty()) {
+            const [top,parent_node] = pq.dequeue()!;
+            const [curr,_] = top;
+            if (visited.has(curr.id)) {
+                continue;
+            } else {
+                visited.add(curr.id);
+            }
+            if (curr.id === end_node.id) {
+                // Reconstruct path by walking parents backward
+                let node_id: number = end_node.id;
+                while (node_id) {
+                    const [_,parent] = distance_map.get(node_id)!;
+                    if (!parent) {
+                        break;
+                    } else {
+                        path.push([this.getNode(node_id),parent]);
+                        node_id = parent.id;
+                    }
+                }
+                break;
+            }
+            processing_order.push([parent_node,curr]);
+
+            const neighbors = this.getNeighbors(curr) ?? [];
+
+            for (const [neighbor,neighbor_weight] of neighbors) {
+                const new_weight = distance_map.get(curr.id)![0] + neighbor_weight;
+                if (new_weight < distance_map.get(neighbor)![0]) {
+                    distance_map.set(neighbor,[new_weight,curr]);
+                    pq.enqueue([[this.getNode(neighbor),neighbor_weight],curr]);
+                }
+            }
+        }
+        // // Reconstruct path by walking parents backward
+        // let node_id: number = end_node.id;
+        // while (node_id) {
+        //     const [_,parent] = distance_map.get(node_id)!;
+        //     if (!parent) {
+        //         break;
+        //     } else {
+        //         path.push([this.getNode(node_id),parent]);
+        //         node_id = parent.id;
+        //     }
+        // }
+
+        const end_time = performance.now();
+        const run_time = end_time-start_time;
+        path.reverse();
+
+        const total_weight = distance_map.get(end_node.id)![0]; 
+        return { processing_order, path, run_time, total_weight };
+    }
+
 };
